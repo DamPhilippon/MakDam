4e9cb1cd289b1f5107b3dc8c77657b2504a23fda
Damien <damien.philippon.dev@gmail.com>
2017-06-11 00:03:55 +0700

    First Display Board

--- src/Pawns/Case.java
+++ src/Pawns/Case.java
@@ -4,5 +4,22 @@ public class Case {
 	private Pawn myPawn;
 	private String myColor;
 	
+	public Case(String aColor)
+	{
+		this.myColor = aColor;
+	}
 	
+	public String getColor()
+	{
+		return this.myColor;
+	}
+	
+	public void setPawn(Pawn aPawn)
+	{
+		this.myPawn = aPawn;
+	}
+	public Pawn getPawn()
+	{
+		return this.myPawn;
+	}
 }
--- src/Resources/IString.java
+++ src/Resources/IString.java
@@ -5,4 +5,6 @@ public class IString {
 	public static String level_medium = "MEDIUM";
 	public static String level_hard = "HARD";
 	public static String level_vs = "VS";
+	public static String white = "WHITE";
+	public static String black = "BLACK";
 }
